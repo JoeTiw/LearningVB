f7e619ae088e42fbd88cca627f0d6ad973d08bbe
Joe Tiwari <bhupintiwari@gmail.com>
2023-09-13 23:23:04 -0500

    first commit

---
+++ VBDemo/Calculator.vb
@@ -0,0 +1,50 @@
+﻿Public Class Calculator
+
+    Public Sub Add(firstNum As Integer, secNum As Integer)
+
+        Dim result As Integer
+        result = firstNum + secNum
+        Console.WriteLine("The addition result is: " & result)
+
+    End Sub
+
+
+    Public Sub Subtract(firstNum As Integer, secondNum As Integer)
+        Dim result As Integer
+        Try
+            result = firstNum - secondNum
+            Console.WriteLine("The Subtraction result is: " & result)
+
+        Catch ex As Exception
+            Console.WriteLine("Error in Subtract: " + ex.Message)
+        End Try
+    End Sub
+
+    Public Sub Multiply(firstNum As Integer, secondNum As Integer)
+
+        Dim result As Integer
+        Try
+            result = firstNum * secondNum
+            Console.WriteLine("The Multipication result is: " & result)
+
+        Catch ex As Exception
+            Console.WriteLine("Error in Multiply: " & ex.Message)
+        End Try
+    End Sub
+
+    Public Sub Divide(firstNum As Decimal, secondNum As Decimal)
+
+        Try
+            Dim result As Decimal
+            result = firstNum / secondNum
+            Console.WriteLine("The divison result is: " & result)
+
+        Catch ex As Exception
+            Console.WriteLine("Error in Divison: " + ex.Message)
+        End Try
+
+    End Sub
+
+
+
+End Class
---
+++ VBDemo/Program.vb
@@ -0,0 +1,14 @@
+Module Program
+    Sub Main(args As String())
+
+        Dim calc As New Calculator()
+        calc.Add(1, 2)
+        calc.Subtract(1, 5)
+        calc.Multiply(2, 3)
+        calc.Divide(10, 2)
+
+
+    End Sub
+End Module
+
+
